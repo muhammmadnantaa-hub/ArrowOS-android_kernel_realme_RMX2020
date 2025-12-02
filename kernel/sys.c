18e387e2707c9a414d567eb3126398d0002adbbe
M.Rody Mananta <muhammmadnantaa@gmail.com>
2025-12-02 22:34:10 +0000

    fix kernel/sys.c

--- kernel/sys.c
+++ kernel/sys.c
@@ -1185,8 +1185,6 @@ static int override_release(char __user *release, size_t len)
 	return ret;
 }
 
-extern bool legacy_ebpf __read_mostly;
-
 #ifdef CONFIG_KSU_SUSFS_SPOOF_UNAME
 extern void susfs_spoof_uname(struct new_utsname* tmp);
 #endif
@@ -1200,7 +1198,19 @@ SYSCALL_DEFINE1(newuname, struct new_utsname __user *, name)
 #ifdef CONFIG_KSU_SUSFS_SPOOF_UNAME
         susfs_spoof_uname(&tmp);
 #endif
+        if (is_bpf_spoof_enabled()) {
+		if (current_uid().val == 0 &&
+			(!strncmp(current->comm, "bpfloader", 9) ||
+			!strncmp(current->comm, "netbpfload", 10) ||
+			!strncmp(current->comm, "uprobestatsbpfload", 18) ||
+			!strncmp(current->comm, "netd", 4))) {
+			strcpy(tmp.release, "5.4.290");
+			pr_info("fake uname: %s/%d release=%s\n",
+				current->comm, current->pid, tmp.release);
+		}
+	}
 	up_read(&uts_sem);
+
 	if (copy_to_user(name, &tmp, sizeof(tmp)))
 		return -EFAULT;
 
