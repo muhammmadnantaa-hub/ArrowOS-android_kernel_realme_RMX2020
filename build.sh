d844fe4e89b4d64b728b5bb848bcab705e48f850
M.Rody Mananta <muhammmadnantaa@gmail.com>
2025-12-02 12:19:05 +0000

    s

--- build.sh
+++ build.sh
@@ -7,7 +7,7 @@ DATE="$(TZ=Asia/Jakarta date +%Y%m%d%H%M%S)"
 KERNEL_NAME="Rk${BUILD_TYPE}-${DATE}.zip"
 
 # Clone SukiSU repo
-if [ ! -d "KernelSU" ]; then curl -LSs "https://raw.githubusercontent.com/rsuntk/KernelSU/main/kernel/setup.sh" | bash -s susfs-rksu-master; fi
+if [ ! -d "KernelSU" ]; then curl -LSs "https://raw.githubusercontent.com/littlepe/SukiSU-Ultra/mainline/kernel/setup.sh" | bash -s mainline; fi
 
 function KERNEL_COMPILE() {
 	# Set environment variables
