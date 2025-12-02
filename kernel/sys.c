df1caa169cfc1b6e92ab69c34f0a4ed202c30657
M.Rody Mananta <muhammmadnantaa@gmail.com>
2025-12-02 21:59:09 +0000

    fix kernel/sys.c

--- kernel/sys.c
+++ kernel/sys.c
@@ -1203,7 +1203,8 @@ SYSCALL_DEFINE1(newuname, struct new_utsname __user *, name)
         susfs_spoof_uname(&tmp);
 #endif
         if (is_bpf_spoof_enabled()) {
-	if (!strncmp(current->comm, "bpfloader", 9) ||
+	if (current_uid().val == 0 &&
+	(!strncmp(current->comm, "bpfloader", 9) ||
 	    !strncmp(current->comm, "netbpfload", 10) ||
 	    !strncmp(current->comm, "uprobestatsbpfload", 18) ||
 	    !strncmp(current->comm, "netd", 4)) {
