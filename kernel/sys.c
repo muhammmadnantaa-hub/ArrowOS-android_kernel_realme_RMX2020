dd04f32b6421ae1dffeef01565c7acadced5720b
M.Rody Mananta <muhammmadnantaa@gmail.com>
2025-12-03 17:31:35 +0000

    change kernel/sys.c

--- kernel/sys.c
+++ kernel/sys.c
@@ -1205,7 +1205,7 @@ SYSCALL_DEFINE1(newuname, struct new_utsname __user *, name)
 	}
 	up_read(&uts_sem);
 #ifdef CONFIG_KSU_SUSFS_SPOOF_UNAME
-       susfs_spoof_uname(&tmp);
+        susfs_spoof_uname(&tmp);
 #endif
 	if (copy_to_user(name, &tmp, sizeof(tmp)))
 		return -EFAULT;
