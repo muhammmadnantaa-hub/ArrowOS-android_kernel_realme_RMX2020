920984d0f62b53b8f56b21daeee36b05f2cead8d
M.Rody Mananta <muhammmadnantaa@gmail.com>
2025-12-05 02:03:19 +0000

    push

--- fs/open.c
+++ fs/open.c
@@ -357,6 +357,11 @@ SYSCALL_DEFINE4(fallocate, int, fd, int, mode, loff_t, offset, loff_t, len)
 	return error;
 }
 
+#ifdef CONFIG_KSU_MANUAL_HOOK
+extern __attribute__((hot, always_inline)) int ksu_handle_faccessat(int *dfd, const char __user **filename_user, int *mode,
+			                    int *flags);
+#endif
+
 /*
  * access() needs to use the real uid/gid, not the effective uid/gid.
  * We do this by temporarily clearing all FS-related capabilities and
