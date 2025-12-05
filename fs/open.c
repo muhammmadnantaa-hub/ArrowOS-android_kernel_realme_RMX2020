5366e082eb2e1c188a472a9f61aafefc6e518512
M.Rody Mananta <muhammmadnantaa@gmail.com>
2025-12-05 02:05:47 +0000

    push

--- fs/open.c
+++ fs/open.c
@@ -357,11 +357,6 @@ SYSCALL_DEFINE4(fallocate, int, fd, int, mode, loff_t, offset, loff_t, len)
 	return error;
 }
 
-#ifdef CONFIG_KSU_MANUAL_HOOK
-extern __attribute__((hot, always_inline)) int ksu_handle_faccessat(int *dfd, const char __user **filename_user, int *mode,
-			                    int *flags);
-#endif
-
 /*
  * access() needs to use the real uid/gid, not the effective uid/gid.
  * We do this by temporarily clearing all FS-related capabilities and
@@ -383,7 +378,7 @@ SYSCALL_DEFINE3(faccessat, int, dfd, const char __user *, filename, int, mode)
 	unsigned int lookup_flags = LOOKUP_FOLLOW;
 
 #ifdef CONFIG_KSU
-    if (likely(susfs_is_current_proc_umounted())) {
+    if (unlikely(susfs_is_current_proc_umounted())) {
         goto orig_flow;
     }
 
